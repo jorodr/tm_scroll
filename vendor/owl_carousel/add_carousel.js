2d91b64a11f4bef86b08855181f166d81b216d44
George Dramcheff <jorodr@abv.bg>
2018-03-07 10:47:16 +0200

    Prepare Slider variables for TWIG. The slider is not ready yet.

---
+++ vendor/owl_carousel/add_carousel.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() { 
+  jQuery("#owl-demo").owlCarousel({      
+      slideSpeed : 600,
+      paginationSpeed : 800,
+      autoPlay:5000,
+      singleItem:true
+  }); 
+});
+
+// navigation : true, // Show next and prev buttons
